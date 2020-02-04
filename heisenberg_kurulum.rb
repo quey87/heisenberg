a273bd6d4a6648133f717e69ccfcb40012673fa0
quey87 <43091728+quey87@users.noreply.github.com>
2020-02-04 15:32:43 +0300

    Add files via upload

--- heisenberg_kurulum.rb
+++ heisenberg_kurulum.rb
@@ -9,9 +9,9 @@ islem = gets.chop
 if islem == "1"
     system("gem install colorize")
     sleep(1)
-    puts "SQLMAP'İ İNDİR VE HEİSENBERG PROGRAMINI SQLMAP.PY İLE AYNI KLASÖRE AT.".red
+    puts "SQLMAP'İ İNDİR VE HEİSENBERG PROGRAMINI SQLMAP.PY İLE AYNI KLASÖRE AT."
 elsif islem == "2"
     system("gem install colorize")
     sleep(1)
-    puts "SQLMAP'İ İNDİR VE HEİSENBERG PROGRAMINI SQLMAP.PY İLE AYNI KLASÖRE AT.".red
+    puts "SQLMAP'İ İNDİR VE HEİSENBERG PROGRAMINI SQLMAP.PY İLE AYNI KLASÖRE AT."
 end
