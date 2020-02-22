ee0e5c99c65cd63bf2d0647e6c8208e985c57ea8
quey87 <43091728+quey87@users.noreply.github.com>
2020-02-22 17:37:03 +0300

    Add files via upload

--- heisenberg_kurulum.rb
+++ heisenberg_kurulum.rb
@@ -1,17 +1,23 @@
 puts"
 1) Windows
 2) Termux
-
+3) Linux
 "
 print "İşlem : "
 islem = gets.chop
 
 if islem == "1"
     system("gem install colorize")
+    system("gem install os")
     sleep(1)
     puts "SQLMAP'İ İNDİR VE HEİSENBERG PROGRAMINI SQLMAP.PY İLE AYNI KLASÖRE AT."
 elsif islem == "2"
     system("gem install colorize")
+    system("gem install os")
     sleep(1)
     puts "SQLMAP'İ İNDİR VE HEİSENBERG PROGRAMINI SQLMAP.PY İLE AYNI KLASÖRE AT."
+elsif islem == "3"
+    system("gem install colorize")
+    system("gem install os")
+    puts "KURULUM TAMAMLANDI !"
 end
